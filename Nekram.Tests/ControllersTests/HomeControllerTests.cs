b7c13d1e912586801d4290ea8a0d2c43bd71e5ee
Macjohnan <jo.jomac.mac801@gmail.com>
2020-01-06 14:20:38 +0300

    Setting Testing environment for Home controller

---
+++ Nekram.Tests/ControllersTests/HomeControllerTests.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+using Nekram.App.Controllers;
+using NUnit.Framework;
+
+namespace Nekram.Tests.ControllersTests {
+
+    [TestFixture()]
+    public class HomeControllerTests {
+
+        private HomeController _controller;
+
+        [SetUp]
+        public void SetUp() { _controller = new HomeController(); }
+
+        [Test]
+        public void Index_HasNoModel_ReturnTrue() {
+            var result = _controller.Index() as ViewResult;
+            // ReSharper disable once PossibleNullReferenceException
+            Assert.AreEqual(null, result.Model);
+        }
+    }
+}
