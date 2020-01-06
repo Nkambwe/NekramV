d362359e4ca320dac76f3c1a27923ce4835bdda6
Macjohnan <jo.jomac.mac801@gmail.com>
2020-01-06 13:23:58 +0300

    Setup Testing environment for application configuration

---
+++ Nekram.Tests/ApplicationModuleTests/AppmoduleTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using Nekram.Models.Application;
+
+namespace Nekram.Tests.ApplicationModuleTests {
+    [TestFixture]
+    public class AppmoduleTests {
+        private Appconfig  _config;
+
+        [SetUp]
+        public void Setup() {
+            _config = new Appconfig();
+        }
+
+        [Test]
+        public void NewConfig_IsValid_ReturnsTrue() {
+            Assert.IsNotNull(_config);
+        }
+    }
+}
