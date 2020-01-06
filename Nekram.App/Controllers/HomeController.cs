144b74e50d0fcfe2f3f0ba989134ae520b6eac8b
Macjohnan <jo.jomac.mac801@gmail.com>
2020-01-06 14:08:17 +0300

    Setting up the Home controller and the home view

---
+++ Nekram.App/Controllers/HomeController.cs
@@ -0,0 +1,10 @@
+﻿using System.Web.Mvc;
+
+namespace Nekram.App.Controllers {
+    public class HomeController : Controller {
+        
+        public ActionResult Index() {
+            return View();
+        }
+    }
+}
