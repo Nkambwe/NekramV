64b388fe0f0363e4d5642e382c8b3d2487aa3d52
Macjohnan <jo.jomac.mac801@gmail.com>
2020-01-07 17:18:04 +0300

    Adding bunddle scipts and styles

---
+++ Nekram.App/App_Start/BundleConfig.cs
@@ -0,0 +1,32 @@
+﻿
+using System.Web.Optimization;
+
+namespace Nekram.App.App_Start {
+    public class BundleConfig {
+
+        public static void RegisterBundles(BundleCollection bundles) {
+
+            //bunddle your script files from here
+            bundles.Add(new ScriptBundle("~/Scripts").Include(
+            "~/Scripts/jquery-3.4.1.js",
+            "~/Scripts/jquery.validate.js",
+            "~/Scripts/jquery.validate.unobtrusive.js",
+            "~/Scripts/bootstrap.js",
+            "~/Scripts/bootstrap.validate.js",
+            "~/Scripts/main.js"));
+
+            bundles.Add(new ScriptBundle("~/Fonts/js").Include("~/Fonts/js/all.js"));
+            bundles.Add(new ScriptBundle("~/Scripts/esm").Include("~/Scripts/esm/popper.js"));
+
+            //bundle style sheets from here
+            bundles.Add(new StyleBundle("~/Content").Include(
+                "~/Content/bootstrap.css",
+                "~/Content/reset.css",
+                "~/Content/main.css"));
+            bundles.Add(new StyleBundle("~/Content/themes").Include(
+                "~/Content/themes/default-Theme.css"));
+            bundles.Add(new StyleBundle("~/Fonts/css").Include(
+                "~/Fonts/css/all.css"));
+        }
+    }
+}
